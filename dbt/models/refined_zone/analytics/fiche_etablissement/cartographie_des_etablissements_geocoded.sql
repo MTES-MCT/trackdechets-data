d518986d074bc1d3b04139985b0daa41ea9e7439
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2025-02-25 10:37:19 +0100

    Adds geocoding

--- dbt/models/refined_zone/analytics/fiche_etablissement/cartographie_des_etablissements_geocoded.sql
+++ dbt/models/refined_zone/analytics/fiche_etablissement/cartographie_des_etablissements_geocoded.sql
@@ -1,10 +1,10 @@
 {{
   config(
     materialized = 'table',
-    )}}
+    ) }}
 
 select
-    siret,
+    c.siret,
     nom_etablissement,
     profils,
     profils_collecteur,
@@ -35,18 +35,21 @@ select
     adresse_insee,
     latitude_td,
     longitude_td,
-    latitude_ban,
-    longitude_ban,
-    cban.latitude                        as latitude_ban,
-    cban.longitude                       as longitude_ban,
-    st_setsrid(
-        st_point(
-            coalesce(c.longitude, cban.longitude),
-            coalesce(c.latitude, cban.latitude)
-        ),
-        4326
-    )                                    as coords,
-from {{ ref("cartographie_des_etablissements") }} c
+    cban.latitude
+        as latitude_ban,
+    cban.longitude
+        as longitude_ban,
+    if(
+        coalesce(c.longitude_td, cban.longitude) is null
+        or coalesce(c.latitude_td, cban.latitude) is null,
+        null,
+        wkt((
+            assumeNotNull(coalesce(c.longitude_td, cban.longitude)),
+            assumeNotNull(coalesce(c.latitude_td, cban.latitude))
+        ))
+    )              as coords,
+    geoToH3(coalesce(c.longitude_td, cban.longitude),coalesce(c.latitude_td, cban.latitude),9) as coords_h3_index
+from {{ ref("cartographie_des_etablissements") }} as c
 left join
     {{ ref("companies_geocoded_by_ban") }} as cban
-    on s.siret = cban.siret and cban.result_status = 'ok'
+    on c.siret = cban.siret and cban.result_status = 'ok'
