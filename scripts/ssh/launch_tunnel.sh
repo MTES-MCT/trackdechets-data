b1b621000ad5c979d59b86a4ec91b3ed0b4f7749
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2025-02-28 09:41:20 +0100

    Updates launch_tunnel

--- scripts/ssh/launch_tunnel.sh
+++ scripts/ssh/launch_tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONFIG_FILE="tunnels.conf"
-SSH_KEY="/opt/ssh_keys/id_scalingo_scaleway_instance"
+CONFIG_FILE="/srv/trackdechets-data/scripts/ssh/tunnels.conf"
+SSH_KEY="/srv/ssh_keys/id_td_platform_data"
 SSH_USER="git"
 SSH_HOST="ssh.osc-secnum-fr1.scalingo.com"
 
@@ -20,13 +20,21 @@ is_tunnel_active() {
     nc -zv 0.0.0.0 ${local_port} &>/dev/null
 }
 
+# Fonction pour vérifier si un tunnel est déjà lancé
+is_tunnel_running() {
+    local local_port=$1
+    ps aux | grep "ssh -L 0.0.0.0:${local_port}:" | grep -v 'grep' > /dev/null
+}
+
 # Lire le fichier de configuration et lancer les tunnels
 while IFS= read -r line; do
     local_port=$(echo $line | awk '{print $1}')
     remote_host=$(echo $line | awk '{print $2}')
     remote_port=$(echo $line | awk '{print $3}')
 
-    launch_tunnel $local_port $remote_host $remote_port
+    if ! is_tunnel_running $local_port; then
+        launch_tunnel $local_port $remote_host $remote_port
+    fi
 done < "$CONFIG_FILE"
 
 # Surveiller les tunnels et les relancer en cas de crash
@@ -37,7 +45,9 @@ while true; do
         if ! is_tunnel_active $local_port; then
             remote_host=$(echo $line | awk '{print $2}')
             remote_port=$(echo $line | awk '{print $3}')
-            launch_tunnel $local_port $remote_host $remote_port
+            if ! is_tunnel_running $local_port; then
+                launch_tunnel $local_port $remote_host $remote_port
+            fi
         fi
     done < "$CONFIG_FILE"
 
