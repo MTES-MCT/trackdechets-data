3d1c91a7227b4080a371d7066930017c7a5f5ca2
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2025-04-10 23:25:03 +0200

    Make non empty stats by naf

--- dbt/models/refined_zone/stats_publiques/stats_annuelles/annual_company_accounts_created_by_naf.sql
+++ dbt/models/refined_zone/stats_publiques/stats_annuelles/annual_company_accounts_created_by_naf.sql
@@ -5,16 +5,16 @@
 
 select
     toYear(ce.created_at)  as annee,
-    ce.code_sous_classe,
-    max(code_section)        as code_section,
-    max(libelle_section)     as libelle_section,
-    max(code_division)       as code_division,
-    max(libelle_division)    as libelle_division,
-    max(code_groupe)         as code_groupe,
-    max(libelle_groupe)      as libelle_groupe,
-    max(code_classe)         as code_classe,
-    max(libelle_classe)      as libelle_classe,
-    max(libelle_sous_classe) as libelle_sous_classe,
+    toNullable(ce.code_sous_classe) as ce.code_sous_classe,
+    toNullable(max(code_section))        as code_section,
+    toNullable(max(libelle_section))     as libelle_section,
+    toNullable(max(code_division))       as code_division,
+    toNullable(max(libelle_division))    as libelle_division,
+    toNullable(max(code_groupe))         as code_groupe,
+    toNullable(max(libelle_groupe))      as libelle_groupe,
+    toNullable(max(code_classe))         as code_classe,
+    toNullable(max(libelle_classe))      as libelle_classe,
+    toNullable(max(libelle_sous_classe)) as libelle_sous_classe,
     count(*)                 as nombre_etablissements
 from
     {{ ref('company_enriched') }} as ce
