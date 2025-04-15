d2905421655b92c9aee7f233f084c669728c6ad3
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2025-04-15 09:34:05 +0200

    Fixes annual_company_accounts_created_by_naf model

--- dbt/models/refined_zone/stats_publiques/stats_annuelles/annual_company_accounts_created_by_naf.sql
+++ dbt/models/refined_zone/stats_publiques/stats_annuelles/annual_company_accounts_created_by_naf.sql
@@ -5,20 +5,21 @@
 
 select
     toYear(ce.created_at)  as annee,
-    toNullable(ce.code_sous_classe) as ce.code_sous_classe,
-    toNullable(max(code_section))        as code_section,
-    toNullable(max(libelle_section))     as libelle_section,
-    toNullable(max(code_division))       as code_division,
-    toNullable(max(libelle_division))    as libelle_division,
-    toNullable(max(code_groupe))         as code_groupe,
-    toNullable(max(libelle_groupe))      as libelle_groupe,
-    toNullable(max(code_classe))         as code_classe,
-    toNullable(max(libelle_classe))      as libelle_classe,
-    toNullable(max(libelle_sous_classe)) as libelle_sous_classe,
+    ce.code_sous_classe as code_sous_classe,
+    max(code_section)       as code_section,
+    max(libelle_section)     as libelle_section,
+    max(code_division)      as code_division,
+    max(libelle_division)    as libelle_division,
+    max(code_groupe)         as code_groupe,
+    max(libelle_groupe)      as libelle_groupe,
+    max(code_classe)         as code_classe,
+    max(libelle_classe)      as libelle_classe,
+    max(libelle_sous_classe) as libelle_sous_classe,
     count(*)                 as nombre_etablissements
 from
     {{ ref('company_enriched') }} as ce
 where
     ce.created_at >= '2020-01-01'
     and ce.created_at < toStartOfWeek(now('Europe/Paris'),1,'Europe/Paris')
+    and not empty(code_sous_classe)
 group by 1,2
