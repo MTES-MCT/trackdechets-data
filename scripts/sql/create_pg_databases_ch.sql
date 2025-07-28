1e44a776e4a5e1297ab0ab45614602bdc345b315
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2025-07-28 10:59:03 +0200

    Updates create_pg_databases script
    As old datawarehouse does not exists anymore.

--- scripts/sql/create_pg_databases_ch.sql
+++ scripts/sql/create_pg_databases_ch.sql
@@ -4,18 +4,3 @@ ENGINE = PostgreSQL('host.docker.internal:10001', 'database_name', 'postgres', '
 
 CREATE DATABASE pg_trackdechets_gerico
 ENGINE = PostgreSQL('host.docker.internal:00003', 'database_name', 'postgres', 'mysecretpassword', 'public',0);
-
-CREATE DATABASE pg_dwh_raw_zone
-ENGINE = PostgreSQL('host.docker.internal:5432', 'trackdechets-datawarehouse', 'postgres', 'mysecretpassword', 'raw_zone',0);
-
-CREATE DATABASE pg_dwh_raw_zone_gerep
-ENGINE = PostgreSQL('host.docker.internal:5432', 'trackdechets-datawarehouse', 'postgres', 'mysecretpassword', 'raw_zone_gerep',0);
-
-CREATE DATABASE pg_dwh_raw_zone_gsheet
-ENGINE = PostgreSQL('host.docker.internal:5432', 'trackdechets-datawarehouse', 'postgres', 'mysecretpassword', 'raw_zone_gsheet',0);
-
-CREATE DATABASE pg_dwh_raw_zone_icpe
-ENGINE = PostgreSQL('host.docker.internal:5432', 'trackdechets-datawarehouse', 'postgres', 'mysecretpassword', 'raw_zone_icpe',0);
-
-CREATE DATABASE pg_dwh_raw_zone_rndts
-ENGINE = PostgreSQL('host.docker.internal:5432', 'trackdechets-datawarehouse', 'postgres', 'mysecretpassword', 'raw_zone_rndts_dlt',0);
