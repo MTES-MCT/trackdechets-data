1694dfdaa23ab1d3645777b7849fb03f29aa2dab
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2025-08-22 10:51:17 +0200

    [airflow] Updates full_update_Search_sirene dag
    Allows to load the dag even with connection or variables not set.

--- airflow/dags/trackdechets_search_sirene/full_update_search_sirene.py
+++ airflow/dags/trackdechets_search_sirene/full_update_search_sirene.py
@@ -10,55 +10,19 @@ from trackdechets_search_sirene.utils import (
     git_clone_trackdechets,
     npm_install_build,
     read_output,
+    get_es_connection,
 )
 from dags_utils.alerting import send_alert_to_mattermost
 
 from airflow.decorators import dag, task
-from airflow.models import Connection, Variable
+from airflow.models import Variable
 from airflow.utils.trigger_rule import TriggerRule
 
 logging.basicConfig()
 logger = logging.getLogger()
 
-es_connection = Connection.get_connection_from_secrets(
-    "trackdechets_search_sirene_elasticsearch_url"
-)
-
-es_credentials = ""
-if es_connection.login and es_connection.password:
-    es_credentials = f"{es_connection.login}:{es_connection.password}@"
-
-es_schema = "http"
-if es_connection.schema:
-    es_schema = f"{es_connection.schema}"
 
 logger = logging.getLogger(__name__)
-
-env = Variable.get("AIRFLOW_ENV", "dev")
-environ = {
-    "FORCE_LOGGER_CONSOLE": True,
-    "ELASTICSEARCH_URL": f"{es_schema}://{es_credentials}{es_connection.host}:{es_connection.port}",
-    "DD_LOGS_ENABLED": True if env == "prod" else False,
-    "DD_TRACE_ENABLED": True if env == "prod" else False,
-    "DD_API_KEY": Variable.get("DD_API_KEY"),
-    "DD_APP_NAME": Variable.get("DD_APP_NAME"),
-    "DD_ENV": "production" if env == "prod" else "",
-    "NODE_ENV": "production" if env == "prod" else "recette",
-    "NODE_OPTIONS": "--max_old_space_size=10240",  # node.js memory allocation
-    "ELASTICSEARCH_CAPEM": Variable.get("ELASTICSEARCH_CAPEM"),
-    "INDEX_CHUNK_SIZE": "2500",
-    "INDEX_SIRET_ONLY": False,
-    "TD_SIRENE_INDEX_MAX_CONCURRENT_REQUESTS": "4",
-    "TD_SIRENE_INDEX_MAX_HIGHWATERMARK": "16384",
-    "TD_SIRENE_INDEX_SLEEP_BETWEEN_CHUNKS": "0",
-}
-
-
-# Constant pointing to the node git indexation repo
-TRACKDECHETS_SIRENE_SEARCH_GIT = "trackdechets-sirene-search"
-TRACKDECHETS_SIRENE_SEARCH_GIT_BRANCH = Variable.get(
-    "TRACKDECHETS_SIRENE_SEARCH_GIT_BRANCH", "main"
-)
 
 
 @dag(
@@ -70,13 +34,16 @@ TRACKDECHETS_SIRENE_SEARCH_GIT_BRANCH = Variable.get(
 def full_update_search_sirene():
     """DAG permettant d'indexer la base SIRENE de l'INSEE dans ElasticSearch"""
 
+    # Constant pointing to the node git indexation repo
+    TRACKDECHETS_SIRENE_SEARCH_GIT = "trackdechets-sirene-search"
+
     @task
     def task_git_clone_trackdechets() -> str:
         tmp_dir = Path(tempfile.mkdtemp(prefix="trackdechets_search_sirene"))
         return git_clone_trackdechets(
             tmp_dir,
             TRACKDECHETS_SIRENE_SEARCH_GIT,
-            TRACKDECHETS_SIRENE_SEARCH_GIT_BRANCH,
+            Variable.get("TRACKDECHETS_SIRENE_SEARCH_GIT_BRANCH", "main"),
         )
 
     @task
@@ -89,7 +56,7 @@ def full_update_search_sirene():
     @task
     def task_download_es_ca_pem(tmp_dir) -> str:
         return download_es_ca_pem(
-            tmp_dir, environ["ELASTICSEARCH_CAPEM"], TRACKDECHETS_SIRENE_SEARCH_GIT
+            tmp_dir, Variable.get("ELASTICSEARCH_CAPEM"), TRACKDECHETS_SIRENE_SEARCH_GIT
         )
 
     @task
@@ -97,6 +64,25 @@ def full_update_search_sirene():
         """
         npm run index
         """
+        airflow_env = Variable.get("AIRFLOW_ENV", "dev")
+
+        environ = {
+            "FORCE_LOGGER_CONSOLE": True,
+            "ELASTICSEARCH_URL": get_es_connection(),
+            "DD_LOGS_ENABLED": True if airflow_env == "prod" else False,
+            "DD_TRACE_ENABLED": True if airflow_env == "prod" else False,
+            "DD_API_KEY": Variable.get("DD_API_KEY"),
+            "DD_APP_NAME": Variable.get("DD_APP_NAME"),
+            "DD_ENV": "production" if airflow_env == "prod" else "",
+            "NODE_ENV": "production" if airflow_env == "prod" else "recette",
+            "NODE_OPTIONS": "--max_old_space_size=10240",  # node.js memory allocation
+            "INDEX_CHUNK_SIZE": "2500",
+            "INDEX_SIRET_ONLY": False,
+            "TD_SIRENE_INDEX_MAX_CONCURRENT_REQUESTS": "4",
+            "TD_SIRENE_INDEX_MAX_HIGHWATERMARK": "16384",
+            "TD_SIRENE_INDEX_SLEEP_BETWEEN_CHUNKS": "0",
+        }
+
         if environ["INDEX_SIRET_ONLY"]:
             command = "npm run index:siret"
         else:
