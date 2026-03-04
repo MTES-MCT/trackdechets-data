6bae67d619ef566d7db51006f739c27112a56fd2
Maxence HAOUARI <maxence.haouari@beta.gouv.fr>
2026-03-04 16:03:20 +0100

    feat(dbt): create new YAML and SQL files for BSDA, BSDD, and BSDASRI revision request approvals

---
+++ dbt/models/trusted_zone/trackdechets/bsdasri_revision_request_approval.sql
@@ -0,0 +1,42 @@
+{{
+  config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = ['id'],
+    on_schema_change='append_new_columns'
+    )
+}}
+
+with source as (
+    select *
+    from {{ source('trackdechets_production', 'bsdasri_revision_request_approval') }} as b
+    {% if is_incremental() %}
+        where
+            b."updatedAt"
+            >= (select toString(toStartOfDay(max(updated_at))) from {{ this }})
+    {% endif %}
+)
+
+select
+    assumeNotNull(
+        toString("id")
+    ) as id,
+    assumeNotNull(
+        toTimeZone(toDateTime64("createdAt", 6), 'UTC')
+    ) as created_at,
+    assumeNotNull(
+        toTimeZone(toDateTime64("updatedAt", 6), 'UTC')
+    ) as updated_at,
+    assumeNotNull(
+        toString("revisionRequestId")
+    ) as revision_request_id,
+    assumeNotNull(
+        toString("approverSiret")
+    ) as approver_siret,
+    toLowCardinality(
+        assumeNotNull(toString("status"))
+    ) as status,
+    toNullable(
+        toString("comment")
+    ) as comment
+from source
